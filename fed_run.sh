9183b896028c1add933419c40adfc42f8539c9a2
iezhuo <iezhuo17@gmail.com>
2022-09-19 00:04:38 +0800

    # federated adapter tuning

--- fed_run.sh
+++ fed_run.sh
@@ -33,6 +33,8 @@ else
 fi
 echo "${task_name}'s max_seq is ${max_seq}"
 
+sleep 7h
+
 CUDA_VISIBLE_DEVICES=${device[0]} python main.py \
 --model_name_or_path ${run_dirs}/pretrain/nlp/roberta-base/ \
 --output_dir ${run_dirs}/output/fedglue \
