5f444d7d387c4089cc2fdd5864b472acf1cbd244
iezhuozhuo <iezhuo17@gmail.com>
2022-09-29 12:07:15 +0800

    # add ner

--- models/classification.py
+++ models/classification.py
@@ -39,14 +39,18 @@ class TokenClassification(BaseModels, ABC):
     def __init__(self, task_name):
         super().__init__(task_name)
 
+        self.num_labels = registry.get("num_labels")
+        self.auto_config = self._build_config(num_labels=self.num_labels)
+        self.backbone = self._build_model()
+
     def _build_model(self):
         backbone = AutoModelForTokenClassification.from_pretrained(
             self.model_config.model_name_or_path,
-            from_tf=bool(".ckpt" in model_args.model_name_or_path),
+            from_tf=bool(".ckpt" in self.model_config.model_name_or_path),
             config=self.auto_config,
             # cache_dir=model_args.cache_dir,
-            revision=model_args.model_revision,
-            use_auth_token=True if model_args.use_auth_token else None,
+            revision=self.model_config.model_revision,
+            use_auth_token=True if self.model_config.use_auth_token else None,
             # ignore_mismatched_sizes=model_args.ignore_mismatched_sizes,
         )
         return backbone
